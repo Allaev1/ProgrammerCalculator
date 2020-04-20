c18124f27ab41a41202fe1fc8feeb8399c2a5938
Bekzod Allaev <Allaev_@outlook.com>
2020-04-20 23:02:45 +0500

    Validator shows errors

--- ProgrammerCalculator/Validators.h
+++ ProgrammerCalculator/Validators.h
@@ -1,59 +1,99 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <Windows.h>
 
 using namespace std;
 
 class Validator // validator for numbers
-{ 
+{
 private:
 	string symbolsForBinary{ "01" };
 	string symbolsForHex{ "0123456789ABCDEF" };
 	string symbolsForDec{ "0123456789" };
 	string symbolsForOctal{ "01234567" };
 
-public:
-	bool IsCorrectBinary(string stringForCheck) 
+	void ShowMarkedErrors(string sourceString, vector<char> wrongSymbols)
 	{
-		for (int i = 0; i < stringForCheck.length(); ++i) 
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+		SetConsoleTextAttribute(hConsole, 7);
+
+		cout << endl << "Error input!" << endl;
+
+		cout << "String with marked error(s):" << endl;
+
+		for (int i = 0; i < sourceString.size();)
+		{
+			for (int a = 0; a < wrongSymbols.size(); a++)
+				if (sourceString[i] == wrongSymbols[a])
+				{
+					SetConsoleTextAttribute(hConsole, 2);
+					cout << sourceString[i];
+					SetConsoleTextAttribute(hConsole, 7);
+					i++;
+				}
+
+			cout << sourceString[i];
+			i++;
+		}
+
+		cout << endl;
+
+		cout << "Please fix error(s)." << endl;
+	}
+
+public:
+	bool IsCorrectBinary(string stringForCheck)
+	{
+		vector<char> wrongSymbols;
+		bool isCorrectBinary = true;//Flag that shows that input binary is correct or no
+
+		for (int i = 0; i < stringForCheck.length(); ++i)
 		{
 			int dismatch = 0; // number of times when symbols didn't match
 
-			for (int k = 0; k < symbolsForBinary.length(); ++k) 
+			for (int k = 0; k < symbolsForBinary.length(); ++k)
 			{
-				if (stringForCheck[i] == symbolsForBinary[k])
-					break; // if the symbols match, go to the next symbol in number
-				else
+				if (stringForCheck[i] != symbolsForBinary[k])
 				{
 					dismatch++;
 
 					if (dismatch == 2) // if the symbol in a number didn't match with all symbols in binary, then return false
 					{
-						cout << "There is invalid symbol found: " << stringForCheck[i] << endl;
-						return false;
+						wrongSymbols.push_back(stringForCheck[i]);
+						isCorrectBinary = false;
 					}
 				}
 			}
+		}
+
+		if (!isCorrectBinary)
+		{
+			ShowMarkedErrors(stringForCheck, wrongSymbols);
+
+			return false;
 		}
 
 		return true;
 	}
 
 	// Same algorithm used below for the rest methods with some little changes. For example, dismatch depends on the base system
-	bool IsCorrectHex(string stringForCheck) 
+	bool IsCorrectHex(string stringForCheck)
 	{
-		for (int i = 0; i < stringForCheck.length(); ++i) 
+		for (int i = 0; i < stringForCheck.length(); ++i)
 		{
 			int dismatch = 0;
 
-			for (int k = 0; k < symbolsForHex.length(); ++k) 
+			for (int k = 0; k < symbolsForHex.length(); ++k)
 			{
 				if (stringForCheck[i] == symbolsForHex[k])
 					break;
-				else 
+				else
 				{
 					dismatch++;
 
-					if (dismatch == 16) 
+					if (dismatch == 16)
 					{
 						cout << "There is invalid symbol found: " << stringForCheck[i] << endl;
 						return false;
@@ -65,21 +105,21 @@ public:
 		return true;
 	}
 
-	bool IsCorrectDec(string stringForCheck) 
+	bool IsCorrectDec(string stringForCheck)
 	{
-		for (int i = 0; i < stringForCheck.length(); ++i) 
+		for (int i = 0; i < stringForCheck.length(); ++i)
 		{
 			int dismatch = 0;
 
-			for (int k = 0; k < symbolsForDec.length(); ++k) 
+			for (int k = 0; k < symbolsForDec.length(); ++k)
 			{
 				if (stringForCheck[i] == symbolsForDec[k])
 					break;
-				else 
+				else
 				{
 					dismatch++;
 
-					if (dismatch == 10) 
+					if (dismatch == 10)
 					{
 						cout << "There is invalid symbol found: " << stringForCheck[i] << endl;
 						return false;
@@ -91,21 +131,21 @@ public:
 		return true;
 	}
 
-	bool IsCorrectOctal(string stringForCheck) 
+	bool IsCorrectOctal(string stringForCheck)
 	{
-		for (int i = 0; i < stringForCheck.length(); ++i) 
+		for (int i = 0; i < stringForCheck.length(); ++i)
 		{
 			int dismatch = 0;
 
-			for (int k = 0; k < symbolsForOctal.length(); ++k) 
+			for (int k = 0; k < symbolsForOctal.length(); ++k)
 			{
 				if (stringForCheck[i] == symbolsForOctal[k])
 					break;
-				else 
+				else
 				{
 					dismatch++;
 
-					if (dismatch == 8) 
+					if (dismatch == 8)
 					{
 						cout << "There is invalid symbol found: " << stringForCheck[i] << endl;
 						return false;
